62d86ee241517db1e5e49b624d0a4ae58cb5aced
Srikar Yallala <srikaryallala@gmail.com>
2020-09-19 12:35:39 -0500

    findchatroom

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Chat from './components/Chat';
+import firebase from './config/Firebase';
 
 export default class App extends Component {
   state = {
@@ -9,18 +10,29 @@ export default class App extends Component {
     chatRoom: null,
   };
 
+  findChatRoom = () => {
+    database = firebase.database();
+    //console.log('completed')
+    var chatRoomRef = database.ref('/');
+    //console.log('completed')
+    chatRoomRef.once('value').then(function(snapshot) {
+      console.log(snapshot.val())
+    })
+    //console.log(database);
+  }
+
   findCoordinates = async () => {
     await navigator.geolocation.getCurrentPosition(
       position => {
         const location = [position.coords.latitude.toFixed(0),position.coords.longitude.toFixed(0)];
         this.setState({location});
+        //console.log(this.state.location);
+        this.findChatRoom();
       },
-      error => Alert.alert(error.message),
+      error => console.log(error.message),
       { enableHighAccuracy: false, timeout: 0, maximumAge: 10000 }
     );
   };
-
-  
 
   render() {
     return ( 
