a54fef7657f37b8436b966af321850abbfe146b5
Srikar Yallala <srikaryallala@gmail.com>
2020-09-19 12:35:09 -0500

    firebasesdk

---
+++ config/firebaseSDK.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  MESSAGE_SENDER_ID,
+  STORAGE_BUCKET,
+  APP_ID,
+  MEASUREMENT_ID,
+} from 'react-native-dotenv'
+
+class FirebaseSDK {
+  constructor() {
+    if (!firebase.apps.length) {
+      //avoid re-initializing
+      firebase.initializeApp({
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        databaseURL: DATABASE_URL,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGE_SENDER_ID,
+      });
+    }
+  }
+}
+const firebaseSDK = new FirebaseSDK();
+export default firebaseSDK;
