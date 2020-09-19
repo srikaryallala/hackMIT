08e5c4e4e9ab93d1b97502793e400fcf0616fe16
Srikar Yallala <srikaryallala@gmail.com>
2020-09-19 10:00:57 -0500

    get location

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar
+} from 'expo-status-bar';
+//import React from 'react';
+import React, {
+  Component
+} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    location: null
+  };
+
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const location = JSON.stringify(position);
+
+        this.setState({location});
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+
+  render() {
+    return ( 
+      <View style = { styles.container } >
+        <TouchableOpacity onPress={this.findCoordinates}>
+          <Text> Location: { this.state.location } </Text> 
+        </TouchableOpacity>
+      </View>
+    );
+
+  }
+
 }
 
 const styles = StyleSheet.create({
