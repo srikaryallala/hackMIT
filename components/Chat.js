30b2446ff445e104f69d0ae7b555745e62dbe4f9
naluconcepcion <lac327@cornell.edu>
2020-09-19 17:02:12 -0400

    user auth fetch

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
-
+import Firebase from '../config/Firebase';
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       messages: ["hi"],
+      user: null,
+      isLoaded: false
     }
   }
 
+  async componentDidMount() {
+    let user = await Firebase.auth().currentUser;
+      if(user) {
+        this.setState({
+          user: user.uid,
+          isLoaded: true,
+        })
+      }
+  }
 
   render() {
-    return ( 
-      <GiftedChat 
-        messages = { this.state.messages }
+    if(this.state.isLoaded) {
+      console.log(this.state.user)
+      return (
+        <GiftedChat
+          messages = { this.state.messages }
 
-        user={{_id: 1}}
-      />
-    );
+          user={{_id: 1}}
+        />
+      );
+    }
+    else {
+      return(
+        null
+      );
+    }
   }
 
 }
